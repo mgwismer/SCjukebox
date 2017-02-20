02b8a916074c68f98b50007a18ca759ae061eef9
Margaret Wismer <margaretgwismer.gmail.com>
2017-02-20 08:55:20 -0500

    add views

--- jukebox.rb
+++ jukebox.rb
@@ -1,16 +1,21 @@
 require 'sinatra'
 require 'SoundCloud'
 
-
 # register a client with YOUR_CLIENT_ID as client_id_
 client = SoundCloud.new(:client_id => ENV['SOUND_CLOUD_API_KEY'])
-# get newest tracks
-tracks = client.get('/tracks', :limit => 10)	
-
-# print each link
-tracks.each do |track|
-  puts track.permalink_url
-end
+# tracks = client.get('/tracks', :limit => 10)
+# # print each link
+# tracks.each do |track|
+#   puts track.permalink_url
+# end
 get '/' do 
-   "Hello World"
+    # get newest tracks
+		@tracks = client.get('/tracks', :limit => 10)	
+		erb :home
+end
+
+#goto the login page
+get '/tracks' do
+	 @tracks = client.get('/tracks', :limit => 10)	
+   erb :home
 end
---
+++ public/scripts/user.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $( ".main-delete-btn" ).click(function() {
+     $('#delete-modal').modal("show");
+  });
+  $(".cancel-btn").click(function(){
+     $('#delete-modal').modal("hide");
+  });
+});
