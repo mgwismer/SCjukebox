ec152f583ac2b3fe64314ba3d708acbd9140fb4b
Margaret Wismer <margaretgwismer@gmail.com>
2017-03-21 14:15:15 -0400

    add require acts_as_list

--- models.rb
+++ models.rb
@@ -1,13 +1,15 @@
+require 'acts_as_list'
+
 class User < ActiveRecord::Base
 	has_many :comments
-	has_many :songs, {order("position ASC")}
+	has_many :songs, -> {order("position ASC")}
 	has_many :colors
 end
 
 class Song < ActiveRecord::Base
 	belongs_to :user
-	acts_as_list :scope, :user
 	has_and_belongs_to_many :comments
+	acts_as_list scope: :user
 end
 
 class Comment < ActiveRecord::Base
